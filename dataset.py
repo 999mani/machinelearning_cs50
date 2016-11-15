874d9a62bce2f243f48faba61319971973f278cd
999mani <manisreekar.sr@gmail.com>
2016-11-15 18:40:45 +0530

    intial code to kick start

---
+++ dataset.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_train = np.array([[1,1],[2,2.5],[3,1.2],[5.5,6.3],[6,9],[7,9]])
+y_train = ['red','red','red','blue','blue','blue']
+
+print(x_train[5,0])
+print(x_train[5,0])
